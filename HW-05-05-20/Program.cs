f974f8946c8380d276a3b6f207d25b691d2465c6
Amirbek2506 <62469848+Amirbek2506@users.noreply.github.com>
2020-05-07 11:24:29 +0500

    создать class MyDictionary<TKey, TValue> (finish)

--- HW-05-05-20/Program.cs
+++ HW-05-05-20/Program.cs
@@ -16,7 +16,7 @@ namespace HW_05_05_20
         public T this[int index]
         {
             get { 
-                if(index>0 && index<myList.Length)
+                if(index>=0 && index<myList.Length)
                 return myList[index];
                 else
                 {
@@ -58,26 +58,114 @@ namespace HW_05_05_20
             return myList.GetEnumerator();
         }
     }
+    class MyDictionary<TKey, TValue>
+    {   private int counter = 0;
+        private TKey[] Keylist = new TKey[0];
+        private TValue[] Valuelist =new TValue[0];
+        public int Counter
+        {
+            get { return this.counter; }
+        }
+
+        public MyDictionary()
+        {
+            TKey[] Keylist = new TKey[0];
+            TValue[] Valuelist = new TValue[0];
+            this.Keylist = Keylist;
+            this.Valuelist = Valuelist;
+        }
+        public void Add(TKey Key, TValue Value)
+        {
+            this.counter++;
+            Array.Resize(ref Keylist, counter);
+            Keylist[counter - 1] = Key;
+
+            Array.Resize(ref Valuelist, counter);
+            Valuelist[counter - 1] = Value;
+        }
+        public TValue this[TKey Key]
+        {
+            get
+            {
+                int index = -1;
+                for (int i = 0; i < counter; i++)
+                {
+                    if (Key.Equals(Keylist[i]))
+                        index = i;
+                }
+                if (index == -1)
+                {
+                    Console.WriteLine(new NullReferenceException().Message);
+                    index++;
+                }
+                return Valuelist[index];
+            }
+            set
+            {
+                for (int i = 0; i < counter; i++)
+                    if (Key.Equals(Keylist[i]))
+                    {
+                        Valuelist[i] = value;
+                    }
+            }
+        }
+        public IEnumerable GetItems()
+        {
+            Pair<TKey, TValue> Item;
+            for (int i = 0; i < counter; i++)
+            {
+                Item = new Pair<TKey, TValue>(Keylist[i], Valuelist[i]);
+                yield return Item;
+            }
+        }
+    }
+    public class Pair<TKey, TValue>
+    {
+        public TKey Key { get; set; }
+        public TValue Value { get; set; }
+
+        public Pair(TKey key, TValue value)
+        {
+            this.Key = key;
+            this.Value = value;
+        }
+    }
  
     class Program
     {
         static void Main(string[] args)
         {
             MyList<string> CityOfTajikistanList = new MyList<string>(22);
+            MyDictionary<int, string> CityDictionarList = new MyDictionary<int, string>();
             CityOfTajikistanList.Add("Душанбе");
             CityOfTajikistanList.Add("Худжанд");
             CityOfTajikistanList.Add("Куляб");
             CityOfTajikistanList.Add("Хоруг");
             CityOfTajikistanList.Add("Вахдат");
             CityOfTajikistanList.Add("Хисор");
-            CityOfTajikistanList.Add("Вандж");
+            CityOfTajikistanList.Add("Вандж");/*
             Console.WriteLine("--------------- GetItemOfIndex --------------------\n");
             Console.WriteLine("CityOfTajikistanList[2] : {0}",CityOfTajikistanList[2]);
             Console.WriteLine("--------------- GetItems ----------------------\n");
             foreach (var City in CityOfTajikistanList)
             Console.Write("{0}  ",City);
             Console.WriteLine("\n\nЕмкость списка: {0} элемент", CityOfTajikistanList.Capacity);
-            Console.WriteLine("Список фактически содержит: {0} элемент", CityOfTajikistanList.Count);
+            Console.WriteLine("Список фактически содержит: {0} элемент", CityOfTajikistanList.Count);*/
+
+
+            //добавим все элемент из CityOfTajikistanList в CityDictionarList
+            for (int i = 0; i < CityOfTajikistanList.Count; i++)
+            {
+                CityDictionarList.Add(i, CityOfTajikistanList[i]);
+            }
+            Console.WriteLine("--------------- GetItemOfKey --------------------\n");
+            for (int i = 0; i < CityDictionarList.Counter; i++)
+            {
+                Console.WriteLine(CityDictionarList[i]);
+            }
+            Console.WriteLine("Key  -  Value \n");
+            foreach (Pair<int, string> KeyValue in CityDictionarList.GetItems())
+                Console.WriteLine(KeyValue.Key+" - "+KeyValue.Value);
             Console.ReadKey();
         }
     }
